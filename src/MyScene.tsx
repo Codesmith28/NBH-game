4232ba03759ddec9141b6d7dde5355eb04e223ac
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-04-22 20:52:30 +0530

    waterfall

--- src/MyScene.tsx
+++ src/MyScene.tsx
@@ -5,60 +5,60 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function MyScene() {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const loader = new GLTFLoader();
-  useEffect(() => {
-    if (!canvasRef.current) return;
+	const loader = new GLTFLoader();
+	useEffect(() => {
+		if (!canvasRef.current) return;
 
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xffffff); // set background color to white
+		const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.setClearColor(0x000000); // set background color to white
 
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      1.1,
-      1000
-    );
+		const camera = new THREE.PerspectiveCamera(
+			75,
+			window.innerWidth / window.innerHeight,
+			1.1,
+			1000
+		);
 
-    // Set camera position to top of the plane
-    // better visibility when reloaded
-    camera.position.set(30, 20, 30);
+		// Set camera position to top of the plane
+		// better visibility when reloaded
+		camera.position.set(30, 30, 30);
 
-    // Set camera orientation to look downwards
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+		// Set camera orientation to look downwards
+		camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    // const scene = new THREE.Scene();
-    // create plane
+		// const scene = new THREE.Scene();
+		// create plane
 
-    loader.load("../blender files/scene.glb", (gltf) => {
-      const scene = gltf.scene;
-      // Do something with the loaded scene
-      // create box
-      // add orbit controls
-      const light = new THREE.DirectionalLight(0xffffff, 2.3);
-      light.position.set(10, 1, 10); // Set the light position
-      scene.add(light);
+		loader.load("../blender files/waterfall.glb", (gltf) => {
+			const scene = gltf.scene;
+			// Do something with the loaded scene
+			// create box
+			// add orbit controls
+			const light = new THREE.DirectionalLight(0xffffff, 2.5);
+			light.position.set(2, 1, 2); // Set the light position
+			scene.add(light);
 
-      const controls = new OrbitControls(camera, canvasRef.current);
+			const controls = new OrbitControls(camera, canvasRef.current);
 
-      controls.enableDamping = true;
-      controls.dampingFactor = 0.05;
-      const animate = () => {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-      };
+			controls.enableDamping = true;
+			controls.dampingFactor = 0.05;
+			const animate = () => {
+				requestAnimationFrame(animate);
+				renderer.render(scene, camera);
+			};
 
-      animate();
+			animate();
 
-      return () => {
-        renderer.dispose();
-      };
-    });
-  }, []);
+			return () => {
+				renderer.dispose();
+			};
+		});
+	}, []);
 
-  return <canvas ref={canvasRef} />;
+	return <canvas ref={canvasRef} />;
 }
 
 export default MyScene;
